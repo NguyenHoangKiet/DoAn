60e3ab2b7fe63f1c3247fa7cadd2a04153d1a73f
NguyenHoangKiet <20520068@gm.uit.edu.vn>
2022-05-03 11:51:27 +0700

    add account main activity

--- app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MainActivity.java
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,126 +37,136 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> account_id,account_username, account_password;
     CustomAdapter customAdapter;
     Account_CustomAdapter accountCustomAdapter;
+    Button account_manage_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        add_button = findViewById(R.id.add_button);
-        empty_imageview = findViewById(R.id.empty_imageview);
-        no_data = findViewById(R.id.no_data);
-        add_button.setOnClickListener(new View.OnClickListener() {
+        account_manage_button = findViewById(R.id.account_manage_button);
+        account_manage_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Account_AddActivity.class);
+                Intent intent = new Intent(MainActivity.this, Account_MainActivity.class);
                 startActivity(intent);
             }
         });
-
-        myDB = new MyDatabaseHelper(MainActivity.this);
-//        myDB.add_account(
-//                "admin0",
-//                "000"
+//
+//        recyclerView = findViewById(R.id.recyclerView);
+//        add_button = findViewById(R.id.add_button);
+//        empty_imageview = findViewById(R.id.empty_imageview);
+//        no_data = findViewById(R.id.no_data);
+//        add_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, Account_AddActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        myDB = new MyDatabaseHelper(MainActivity.this);
+////        myDB.add_account(
+////                "admin0",
+////                "000"
+////        );
+////        myDB.add_account(
+////                "admin1",
+////                "111"
+////        );
+////        myDB.add_account(
+////                "admin2",
+////                "222"
+////        );
+//
+//        thamso_tenthamso = new ArrayList<>();
+//        thamso_giatri = new ArrayList<>();
+//
+//        account_id = new ArrayList<>();
+//        account_password = new ArrayList<>();
+//        account_username = new ArrayList<>();
+//
+//        storeDataInArrays();
+//
+//        customAdapter = new CustomAdapter(MainActivity.this,this,
+//                thamso_tenthamso,
+//                thamso_giatri
 //        );
-//        myDB.add_account(
-//                "admin1",
-//                "111"
-//        );
-//        myDB.add_account(
-//                "admin2",
-//                "222"
-//        );
-
-        thamso_tenthamso = new ArrayList<>();
-        thamso_giatri = new ArrayList<>();
-
-        account_id = new ArrayList<>();
-        account_password = new ArrayList<>();
-        account_username = new ArrayList<>();
-
-        storeDataInArrays();
-
-        customAdapter = new CustomAdapter(MainActivity.this,this,
-                thamso_tenthamso,
-                thamso_giatri
-        );
-
-        accountCustomAdapter = new Account_CustomAdapter(MainActivity.this,this,
-                account_id,
-                account_username,
-                account_password);
-
-        recyclerView.setAdapter(accountCustomAdapter);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+//
+//        accountCustomAdapter = new Account_CustomAdapter(MainActivity.this,this,
+//                account_id,
+//                account_username,
+//                account_password);
+//
+//        recyclerView.setAdapter(accountCustomAdapter);
+//
+//        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1){
-            recreate();
-        }
-    }
-
-    void storeDataInArrays(){
-        Cursor cursor = myDB.read_all_account();
-//        Cursor cursor = myDB.readAllData();
-        if(cursor.getCount() == 0){
-            empty_imageview.setVisibility(View.VISIBLE);
-            no_data.setVisibility(View.VISIBLE);
-        }else{
-            while (cursor.moveToNext()){
-                account_id.add(cursor.getString(0));
-                account_username.add(cursor.getString(1));
-                account_password.add(cursor.getString(2));
-//                thamso_tenthamso.add(cursor.getString(0));
-//                thamso_giatri.add(cursor.getString(1));
-            }
-            empty_imageview.setVisibility(View.GONE);
-            no_data.setVisibility(View.GONE);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.my_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.delete_all){
-            confirmDialog();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    void confirmDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Delete All?");
-        builder.setMessage("Are you sure you want to delete all Data?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                MyDatabaseHelper myDB = new MyDatabaseHelper(MainActivity.this);
-//                myDB.deleteAllData();
-                //Refresh Activity
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-        builder.create().show();
-    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == 1){
+//            recreate();
+//        }
+//    }
+//
+//    void storeDataInArrays(){
+//        Cursor cursor = myDB.read_all_account();
+////        Cursor cursor = myDB.readAllData();
+//        if(cursor.getCount() == 0){
+//            empty_imageview.setVisibility(View.VISIBLE);
+//            no_data.setVisibility(View.VISIBLE);
+//        }else{
+//            while (cursor.moveToNext()){
+//                account_id.add(cursor.getString(0));
+//                account_username.add(cursor.getString(1));
+//                account_password.add(cursor.getString(2));
+////                thamso_tenthamso.add(cursor.getString(0));
+////                thamso_giatri.add(cursor.getString(1));
+//            }
+//            empty_imageview.setVisibility(View.GONE);
+//            no_data.setVisibility(View.GONE);
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.my_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if(item.getItemId() == R.id.delete_all){
+//            confirmDialog();
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    void confirmDialog(){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Delete All?");
+//        builder.setMessage("Are you sure you want to delete all Data?");
+//        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                MyDatabaseHelper myDB = new MyDatabaseHelper(MainActivity.this);
+////                myDB.deleteAllData();
+//                //Refresh Activity
+//                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//
+//            }
+//        });
+//        builder.create().show();
+//    }
 }
